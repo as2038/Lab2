2b43ee10f7cc8b06d55ff86cb290c1632fd7d2fb
as2038 <90843703+as2038@users.noreply.github.com>
2021-11-03 17:35:00 +0400

    Added the Bee class and createBeeImg function.

--- game.js
+++ game.js
@@ -45,6 +45,8 @@ function Bear () {
 function start() {
 	bear = new Bear();
 	document.addEventListener("keydown", moveBear, false);	
+
+
 }
 
 
@@ -69,5 +71,72 @@ function moveBear(e) {
 	if (e.keyCode == KEYDOWN) {
 		bear.move(0, 1)
 	}
+}
+
+class Bee {
+
+	constructor(beeNumber) {
+
+		this.htmlElement = createBeeImg(beeNumber);
+		this.id = this.htmlElement.id;
+		this.x = this.htmlElement.offsetLeft;
+		this.y = this.htmlElement.offsetTop;
+
+		this.move = function(dx, dy) {
+			this.x += dx;
+			this.y += dy;
+			this.display();
+		};
+
+		this.display = function() {
+			this.fitBounds();
+			this.htmlElement.style.left = this.x + "px";
+			this.htmlElement.style.top = this.y + "px";
+			this.htmlElement.style.display = "block";
+		};
+
+		this.fitBounds = function() {
+			let parent = this.htmlElement.parentElement;
+			let iw = this.htmlElement.offsetWidth;
+			let ih = this.htmlElement.offsetHeight;
+			let l = parent.offsetLeft;
+			let t = parent.offsetTop;
+			let w = parent.offsetWidth;
+			let h = parent.offsetHeight;
+
+			if (this.x < 0) this.x = 0;
+			if (this.x > w - iw) this.x = w - iw;
+			if (this.y < 0) this.y = 0;
+			if (this.y > h - ih) this.y = h - ih;
+		};
+	}
+}
+
+
+
+function createBeeImg(wNum) {
+	let boardDiv = document.getElementById("board");
+	let boardDivW = boardDiv.offsetWidth;
+	let boardDivH = boardDiv.offsetHeight;
+	let boardDivX = boardDiv.offsetLeft;
+	let boardDivY = boardDiv.offsetTop;
+
+	let img = document.createElement("img");
+	img.setAttribute("src", "images/bee.gif");
+	img.setAttribute("width", "100");
+	img.setAttribute("alt", "A bee!");
+	img.setAttribute("id", "bee" + wNum);
+	img.setAttribute("class", "bee");
+
+	img.style.position = "absolute";
+
+	boardDiv.appendChild(img);
+
+	let x = getRandomInt(boardDivW);
+	let y = getRandomInt(boardDivH);
+	img.style.left = (boardDivX + x) + "px";
+	img.style.top = (y) + "px"; 
+
+	return img;
 }
 
